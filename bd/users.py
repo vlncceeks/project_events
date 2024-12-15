3ce3d74bb4e5fbbd2b09886d25f3ff54eb7a9f7a
Kirill <kurumi8927@gmail.com>
2024-12-15 21:34:39 +0300

    добавил 3 юзеров в таблицу(потом удалю), сделал запросы для получения данных из таблиц

--- bd/users.py
+++ bd/users.py
@@ -15,3 +15,38 @@ cursor.execute('''
 connection.commit()
 connection.close()
 print("База данных и таблица успешно созданы!")
+
+#-----------------------------------------------------------
+import sqlite3
+import hashlib
+
+# Функция для хеширования пароля
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
+
+# Данные для добавления
+users_data = [
+    ('user1', 'password1'),
+    ('user2', 'password2'),
+    ('user3', 'password3')
+]
+
+# Подключаемся к базе данных
+connection = sqlite3.connect("events.db")
+cursor = connection.cursor()
+
+# Добавляем пользователей в таблицу
+for login, password in users_data:
+    password_hash = hash_password(password)
+    cursor.execute('''
+        INSERT INTO users (login, password_hash)
+        VALUES (?, ?)
+    ''', (login, password_hash))
+
+# Сохраняем изменения в базе данных
+connection.commit()
+
+# Закрываем соединение
+connection.close()
+
+print("Данные успешно добавлены!")
