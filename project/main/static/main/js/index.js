96d8e38b6c598b219206240f904394a31e0c65d4
Ксения <vlncceeks@gmail.com>
2024-12-18 21:01:55 +0300

    правки html

--- project/main/static/main/js/index.js
+++ project/main/static/main/js/index.js
@@ -1,4 +1,4 @@
-const wrapper = document.querySelector(".wrapper__container");
+const wrapper = document.querySelector(".wrapper__activity");
 const search = document.querySelector(".search__input");
 const URL_API = "http://127.0.0.1:8000/api/events/";
 
@@ -22,28 +22,28 @@ function showEvent(data) {
   data.forEach((event) => {
     const eventElement = document.createElement("div");
 
-    eventElement.classList.add("wrapper__activity", "activity");
+    eventElement.classList.add("activity__event", "event");
 
     eventElement.innerHTML = `
-        <div class="activity__event event">
-            <div class="event__image">
-            <img src="images/${event.photo}" alt="event">
+        
+            <div class="event__container_image">
+              <img src="images/${event.photo}" alt="event" class="event__image">
             </div>
             <div class="events__content content">
               <details class="content__details details">
                 <summary class="details__title">${event.title}</summary>
                 <p class="details__description">___________________________</p>
-                <p class="details__materials_title">${event.materials}</p>
+                <p class="details__materials_title">${event.materials.replace('"', '').replace('"', '')}</p>
                 <p class="details__materials">___________________________</p>
               </details>
               <p class="content__authors">___________________________</p>
-              <p class="content__date_and_time">${event.date_time}</p>
-              <p class="content__numbers_of_seats">${event.places}</p>
+              <p class="content__date_and_time">${event.date_time.replace('T', ' ').replace('Z', ' ').replace('-', '.').replace('-', '.')}</p>
+              <p class="content__numbers_of_seats">Количество мест: ${event.places}</p>
               <button class="content__sign_button sign_button">
                 Записаться
               </button>
             </div>
-          </div>
+          
         `;
     wrapper.appendChild(eventElement);
   });
